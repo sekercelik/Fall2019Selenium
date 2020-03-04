f4784ce66945e8b104b854c110ee2bf5c6ffdd4f
sekercelik <sceliksidika@gmail.com>
2020-03-03 20:35:16 -0500

    self basic navigation

---
+++ src/test/java/com/automation/utilities/DriverFactory.java
@@ -0,0 +1,33 @@
+package com.automation.utilities;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class DriverFactory {
+//we are writing same codes again and again
+//we will write a method and call it : DRY Principle
+//sth that support our code : utilities
+//under automation package
+//We created utilities package and DriverFactory class under automation package
+//we wrote reusable methods inside DriverFactory class named => createDriver
+//whenever we need to crate webDriver object
+//we will call our custom method
+    /*  this method returns webdriver object based on browser type
+    * if you want to use chrome browser, just provide chrome as a parameter
+    * * @param browserName
+     * @return webdriver object
+     * */
+
+    public static WebDriver createADriver(String browserName){
+        if(browserName.equalsIgnoreCase("chrome")){
+            WebDriverManager.chromedriver().setup();
+            return new ChromeDriver();
+        }else{
+            WebDriverManager.firefoxdriver().setup();
+            return new FirefoxDriver();
+        }
+    }
+
+}
